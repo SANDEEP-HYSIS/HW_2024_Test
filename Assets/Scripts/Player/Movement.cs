a5b856d79470e4e161b287e81db9808dfb5df19d
SANDEEP-HYSIS <111502184+SANDEEP-HYSIS@users.noreply.github.com>
2024-01-22 18:47:02 +0530

    environmentSetup

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody player;
+    private float movespeed = 3f;
+    private float turnSpeed = 100f;
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(Vector3.up*turnSpeed*Input.GetAxis("Horizontal")*Time.deltaTime);
+       transform.Translate(0f,0f,movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
